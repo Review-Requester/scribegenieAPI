209b9f610016e44a2792938e02207bcaecba8f92
JinalEmpiricTech <jinal@empiricinfotech.com>
2024-01-24 06:05:45 +0000

    Update setting file

--- azure_api/settings.py
+++ azure_api/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+
 ALLOWED_HOSTS = os.environ.get('SERVERNAMES').split(',')
+
 
 # Application definition
 
